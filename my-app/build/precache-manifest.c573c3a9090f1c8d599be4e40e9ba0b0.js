e27bdbd788a8ea280bf26ad62bb274822859add7
csporto23 <Csporto23@gmail.com>
2019-07-10 18:01:52 -0500

    netlify setup

---
+++ my-app/build/precache-manifest.c573c3a9090f1c8d599be4e40e9ba0b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14140c61485d586b625f03087e2cf8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a8d743a86c8b63a788c",
+    "url": "/static/css/main.7eedff02.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "7a8d743a86c8b63a788c",
+    "url": "/static/js/main.17354fa6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
