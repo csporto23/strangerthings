476deba1ea582f518958c7ad7f6d82e6fe943ff8
csporto23 <Csporto23@gmail.com>
2019-07-12 20:04:54 -0500

    stranger state added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import FriendCard from "./components/StrangerCard";
+import StrangerCard from "./components/StrangerCard";
 import Wrapper from "./components/Wrapper";
 import strangers from "./stranger.json";
 import Navbar from "./components/Navbar";
@@ -11,13 +11,6 @@ class App extends Component {
     strangers
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const strangers = this.state.strangers.filter(stranger => stranger.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ strangers });
-  };
-
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
@@ -26,11 +19,7 @@ class App extends Component {
     <Header></Header>
    <Wrapper>
         {this.state.strangers.map(strangers => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={strangers.id}
-            key={strangers.id}
-            name={strangers.name}
+          <StrangerCard
             image={strangers.image}
           />
         ))}
